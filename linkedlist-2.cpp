cf98d44e526b5ca6141d1ed00753c5f12dcb3b0d
Simran Kaur <simkaur2637@gmail.com>
2026-09-14 01:15:23 +0530

    Create linkedlist-2.cpp
    this is how to recognize the pattern

---
+++ linkedlist-2.cpp
@@ -0,0 +1,30 @@
+How to Recognize the Pattern
+********"Find middle"
+
+→ Slow + Fast
+
+********"Detect cycle"
+
+→ Slow + Fast
+
+**********"Find nth node from end"
+
+→ Two pointers with a gap
+
+**********"Reverse linked list"
+
+→ Prev + Current + Next
+
+************"Merge sorted linked lists"
+
+→ Two pointers
+
+***********"Remove nodes"
+
+→ Pointer manipulation
+
+********"Palindrome linked list"
+
+→ Middle + Reverse + Compare
+
+  
