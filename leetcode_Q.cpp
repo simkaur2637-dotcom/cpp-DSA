65420ceccf64798fa58450423710a75e5fbc393c
Simran Kaur <simkaur2637@gmail.com>
2026-09-14 23:46:55 +0530

    Create leetcode_Q.cpp
    leetcode questions on the topic of queue

---
+++ leetcode_Q.cpp
@@ -0,0 +1,25 @@
+
+//933 — Number of Recent Calls
+#include <queue>
+using namespace std;
+
+class RecentCounter {
+private:
+    queue<int> q;
+
+public:
+
+    RecentCounter() {
+    }
+
+    int ping(int t) {
+
+        q.push(t);
+
+        while (q.front() < t - 3000) {
+            q.pop();
+        }
+
+        return q.size();
+    }
+};
